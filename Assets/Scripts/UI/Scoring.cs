c8890a12b808f343e4c41cc263d4724cf33fb91c
Shubham <40598861+vijit101@users.noreply.github.com>
2019-08-16 17:16:36 +0530

    -UI Lives and Score Updates Now

--- Assets/Scripts/UI/Scoring.cs
+++ Assets/Scripts/UI/Scoring.cs
@@ -1,18 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Scoring : MonoBehaviour
 {
+    public TextMeshProUGUI Lives, Score,Gameover;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Lives.text = "Lives :"+PlayerPrefs.GetInt("Lives").ToString();
+        Score.text = "Score :" + PlayerPrefs.GetFloat("Score").ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Lives.text = "Lives :" + PlayerPrefs.GetInt("Lives").ToString();
+        Score.text = "Score :" + PlayerPrefs.GetFloat("Score").ToString();
+        if(PlayerPrefs.GetInt("Lives") == 0)
+        {
+            Gameover.enabled = true;
+        }
     }
 }
